3c527fb3582284ac228ec6d3061f2d5869d0f2b1
karwo001 <karwo001@hunn2-107.ad.liu.se>
2019-12-10 10:46:14 +0100

    Labb 3 half done

---
+++ Lab3/ParametricCurve.m
@@ -0,0 +1,11 @@
+function [sx, sy] = ParametricCurve(x,y)
+   t = 0:1/(length(x)-1):1;
+   sx = csape(t,x);
+   sy = csape(t,y);
+   n = 10*length(x);
+   tt = (0:n)/n;
+   plot( ppval(sx,tt) , ppval(sy,tt),'-b')
+   hold on;
+   plot( x,y,'r+')
+   hold off;
+end
